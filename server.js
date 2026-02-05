fa2df252f8e352a26a0297ddd3633b6f25a376cf
lynnvdbo <lynn.van.den.bosch@hva.nl>
2026-02-05 10:24:28 +0100

    Custom data property toegevoegd #1

--- server.js
+++ server.js
@@ -14,10 +14,12 @@ const personResponse = await fetch('https://fdnd.directus.app/items/person/' + p
 
 // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
 const personResponseJSON = await personResponse.json()
+const personData = personResponseJSON.data
+personData.custom = JSON.parse(personData.custom)
 
 // Controleer eventueel de data in je console
 // (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
-// console.log(personResponseJSON)
+console.log(personResponseJSON)
 
 
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
